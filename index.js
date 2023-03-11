78e85df2d5ba9a5f8df4c409659e8092b0a1d1bb
Abdur Rahim <124195272+105rahim@users.noreply.github.com>
2023-03-11 04:45:34 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#search").click(function(){
+        $(".form-control").slideToggle("slow");
+    });
+
+    $("#search").click(function(){
+        $("#btn").slideToggle("slow");
+    });
+
+    $("#profile").click(function(){
+        $("form").slideToggle("slow");
+    });
+
+   
+
+    
+});
